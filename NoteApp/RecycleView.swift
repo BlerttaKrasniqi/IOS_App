d4dc7c72c0b5f346c122c988f422b7f5bb8bfcc4
dinapirana <122113432+dinapirana@users.noreply.github.com>
2024-02-28 22:47:09 +0100

    Restore deleted note functionality added

--- NoteApp/RecycleView.swift
+++ NoteApp/RecycleView.swift
@@ -137,6 +137,49 @@ class RecycleView: UIViewController, UITableViewDataSource, UITableViewDelegate 
             print("Failed to delete notes permanently: \(error)")
         }
     }
+     @IBAction func restoreAllButtonTapped(_ sender: UIButton) {
+
+        let alertController = UIAlertController(title: "Restore All", message: "Are you sure you want to restore all notes?", preferredStyle: .alert)
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let restoreAction = UIAlertAction(title: "Restore", style: .default) { _ in
+            self.performRestoreAll()
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(restoreAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    private func performRestoreAll() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+
+        let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+
+        do {
+            // Fetch all notes with deletedDate not nil
+            let request = NSFetchRequest<Note>(entityName: "Note")
+            request.predicate = NSPredicate(format: "deletedDate != nil")
+            let notesToRestore = try context.fetch(request)
+
+           // Restore each note
+            for note in notesToRestore {
+                note.deletedDate = nil
+            }
+
+            // Save changes to Core Data
+            try context.save()
+
+            // Update the deletedNotes array (remove all elements)
+            deletedNotes.removeAll()
+
+            // Reload the table view to reflect the changes
+            tableView.reloadData()
+            print("All notes restored successfully.")
+        } catch {
+            print("Failed to restore notes: \(error)")
+        }
+    }
 }
  
 
