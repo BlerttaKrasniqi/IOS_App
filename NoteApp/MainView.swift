1d766cf63f02885638240f11a8768298041bd977
BlerttaKrasniqi <122098017+BlerttaKrasniqi@users.noreply.github.com>
2024-02-29 16:13:02 +0100

    final editing

--- NoteApp/MainView.swift
+++ NoteApp/MainView.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 class MainView: UIViewController {
+    @IBOutlet weak var welcomeLabel: UILabel!
+
+    @IBOutlet weak var startbutton: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
